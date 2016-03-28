5ae2d6f2513f58a2fafbe556bb51be9d759b34d5
Omri Dor <omridor@gmail.com>
2016-03-29 00:24:06 +0300

    Added form nav bar

--- client/js/controllers/form-controller.js
+++ client/js/controllers/form-controller.js
@@ -6,12 +6,14 @@ angular.module('borderlineApp').controller('FormController',['$scope', '$resourc
   // View model alias indicates what should be exposed in html.
   var vm = this;
 
-  // Defines form step order, their state names (for ui-router navigation), their fields (for formly form generation) and whether they are known to be valid. This does not
-  // ensure validity, but gives an indication that the form was valid as the user navigated out of it.
+  // Defines form step order, their state names (for ui-router navigation), their fields (for formly form generation) and valid/visited flags. The valid flag
+  // retains the last known state of the step right before we navigated away from it.
   vm.steps = [
     {
       stepName: 'general',
+      stepDisplayName: 'General',
       valid: false,
+      visited: false,
       fields: [
         {
           key: 'dateOfIncident',
@@ -54,12 +56,16 @@ angular.module('borderlineApp').controller('FormController',['$scope', '$resourc
     },
     {
       stepName: 'location',
+      stepDisplayName: 'Location',
       valid: false,
+      visited: false,
       fields: []
     },
     {
       stepName: 'ids',
+      stepDisplayName: 'IDs',
       valid: false,
+      visited: false,
       fields: [
         {
           key: 'email',
@@ -82,42 +88,73 @@ angular.module('borderlineApp').controller('FormController',['$scope', '$resourc
     },
     {
       stepName: 'confirm',
+      stepDisplayName: 'Confirm',
       valid: false,
+      visited: false,
       fields: []
     },
 
   ];
 
-  // Save validity of form step when the user leaves the step. Necessary b/c when form step is removed from
-  // DOM it's no longer possible to access $valid. This covers hitting back or manually navigating into a form step.
+  // Set flags on out-of-DOM form steps.
+  // Visited flag applies to all visited or skipped steps (errors will be indicated on nav bar with this).
+  // Valid flag assigned when user leaves the form step, even if user does so with back button
+  // or manual navigation.
   $rootScope.$on('$stateChangeStart', 
   function(event, toState, toParams, fromState, fromParams, options){
-    vm.saveValidityOfStep(fromState.name); 
-  });
-
-  vm.saveValidityOfStep = function(stepName) {
-    var stepIndex = vm.steps.findIndex(function(element) {
-      return element.stepName === stepName; 
-    });
-    if (stepIndex === -1) {
-      // We are not leaving from one of the form steps, nothing to do
-      return;
+    var fromStepIndex = vm.findStepIndexByStateName(fromState.name);
+    if (fromStepIndex > -1) {
+      vm.saveValidityOfStep(fromStepIndex);
     }
 
+    // Mark all previous steps as visited, even if they were skipped.
+    // Not that final step (confirm) never gets this flag set. This is
+    // by design.
+    var toStepIndex = vm.findStepIndexByStateName(toState.name);
+    for (var i = 0; i < toStepIndex; i++) {
+      vm.steps[i].visited = true;
+    }
+  });
+
+  vm.findStepIndexByStateName = function(stateName) {
+    return vm.steps.findIndex(function(element) {
+      return element.stepName === stateName; 
+    });
+  };
+
+  // Copy validity state of form into steps array.
+  vm.saveValidityOfStep = function(stepIndex) {
     if (!vm.steps[stepIndex].form) {
       // If step has no form (i.e. informational), just mark as valid to indicate user has seen it.
       vm.steps[stepIndex].valid = true;
-    } else if (vm.steps[stepIndex].form.$valid) {
-      vm.steps[stepIndex].valid = true;
     } else {
-      // Remember that the step was not valid. This will disallow form submission.
-      vm.steps[stepIndex].valid = false;
+      vm.steps[stepIndex].valid = vm.steps[stepIndex].form.$valid;
     }
+  };
+
+  vm.getClass = function (step) {
+    if (!step) {
+      return '';
+    }
+    if (step.stepName === $state.current.name) {
+      return 'active';
+    }
+
+    if (step.visited === false) {
+      return 'unvisited';
+    }
+    return (step.valid) ? 'valid': 'invalid';
+  };
+
+  vm.shouldDrawFormNav = function() {
+    return vm.steps.some(function (step, index) {
+      return step.stepName === $state.current.name;
+    });
   };
 
   vm.submitReport = function() {
     // Save current step validity into same structure as all other steps, then check validity of all of them.
-    vm.saveValidityOfStep($state.current.name);
+    vm.saveValidityOfStep(vm.findStepIndexByStateName($state.current.name));
     for (var index in vm.steps) {
       if (!vm.steps[index].valid) {
         dialogs.error('Incomplete form', 'Please go back and complete previous form steps');
